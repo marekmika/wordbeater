353e764f9b46cdd98f7b3e5815c3637a6c1f17ce
Marek Mika <marek.mika@inventi.cz>
2020-06-17 22:32:02 +0200

    Added Bootstrap

---
+++ wordbeater/pages/game.js
@@ -0,0 +1,11 @@
+import React, { useState } from "react";
+
+const GamePage = () => {
+    return (
+        <div className="col-lg-8 mx-auto">
+            <span>Hello</span>
+        </div>
+    )
+};
+
+export default GamePage
